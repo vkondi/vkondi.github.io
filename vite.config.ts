c0d8f382829eef3f635ade345fe0de677fc8d55f
Vishwajeet Kondi <vkondi@gmail.com>
2025-05-29 18:43:52 +0530

    fix: vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vkondi.github.io/', // Add base URL for GitHub Pages
+  base: '/', // Use root path for all environments
   build: {
     outDir: 'dist',
     rollupOptions: {
