f9d604ae5b17181e9e81465e4e7e0eeccd5cef3e
Vishwajeet Kondi <vkondi@gmail.com>
2025-05-29 16:58:07 +0530

    feat: deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vkondi.github.io/', // Add base URL for GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
