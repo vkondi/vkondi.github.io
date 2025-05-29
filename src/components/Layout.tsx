aa177127999bfb88d2172c9814271c29b690624f
Vishwajeet Kondi <vkondi@gmail.com>
2025-05-29 19:14:28 +0530

    fix: main component center alignment

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,7 @@ import {
   IconButton,
   Typography,
   Container,
+  Box,
 } from "@mui/material";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
 
@@ -16,9 +17,22 @@ interface LayoutProps {
 
 const Layout = ({ children, toggleTheme, isDarkMode }: LayoutProps) => {
   return (
-    <>
-      <AppBar position="static" elevation={0} color="transparent">
-        <Toolbar sx={{ justifyContent: 'space-between', px: { xs: 2, sm: 4, md: 6 } }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        width: "100vw",
+      }}
+    >
+      <AppBar
+        position="static"
+        elevation={0}
+        color="transparent"
+      >
+        <Toolbar
+          sx={{ justifyContent: "space-around", px: { xs: 2, sm: 4, md: 6 } }}
+        >
           <Typography variant="h6" component="div">
             Resume
           </Typography>
@@ -27,30 +41,30 @@ const Layout = ({ children, toggleTheme, isDarkMode }: LayoutProps) => {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Container 
-        disableGutters 
-        sx={{ 
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          minHeight: 'calc(100vh - 64px)', // Subtract AppBar height
+      <Container
+        disableGutters
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "calc(100vh - 64px)", // Subtract AppBar height
           py: 4,
         }}
       >
         <Container
           maxWidth="md"
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             px: { xs: 2, sm: 3, md: 4 },
           }}
         >
           {children}
         </Container>
       </Container>
-    </>
+    </Box>
   );
 };
 
